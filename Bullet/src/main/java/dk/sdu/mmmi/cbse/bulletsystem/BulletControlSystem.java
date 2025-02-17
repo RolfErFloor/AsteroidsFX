b11c152093b589eca655073a5584aeb801decf2c
RolfErFloor <rolf.vandsted@gmail.com>
2025-02-17 20:18:34 +0100

    Bullets removed when hit below 0 on xy and above display widht and height

--- Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletControlSystem.java
+++ Bullet/src/main/java/dk/sdu/mmmi/cbse/bulletsystem/BulletControlSystem.java
@@ -17,7 +17,26 @@ public class BulletControlSystem implements IEntityProcessingService, BulletSPI 
             double changeY = Math.sin(Math.toRadians(bullet.getRotation()));
             bullet.setX(bullet.getX() + changeX * 3);
             bullet.setY(bullet.getY() + changeY * 3);
+
+            if (bullet.getX() < 0){
+                world.removeEntity(bullet);
+                System.out.println("Removed Bullet");
+            }
+            if (bullet.getY() < 0){
+                world.removeEntity(bullet);
+                System.out.println("Removed Bullet");
+            }
+            if (bullet.getX() > gameData.getDisplayWidth()){
+                world.removeEntity(bullet);
+                System.out.println("Removed Bullet");
+            }
+            if (bullet.getY() > gameData.getDisplayHeight()){
+                world.removeEntity(bullet);
+
+                System.out.println("Removed Bullet");
+            }
         }
+
     }
 
     @Override
@@ -31,5 +50,8 @@ public class BulletControlSystem implements IEntityProcessingService, BulletSPI 
         bullet.setRotation(shooter.getRotation());
         bullet.setRadius(1);
         return bullet;
+
     }
+
+
 }
